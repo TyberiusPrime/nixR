1f3e2f9ac5d325c7eac2eefd6e6a2265449dda12
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2022-12-02 13:13:57 +0100

    exporting all rPackages, part2

--- nix/r/wrapper.nix
+++ nix/r/wrapper.nix
@@ -1,69 +1,78 @@
-{ stdenv, R, makeWrapper, recommendedPackages, packages, lndir, nixpkgs, lib, system }:
-
-builtins.derivation { 
-# we don't want to use a stdenv
-# for that pulls in setupHooks
-# and at least the cmake hook
-# will take a long time
-# and build a giant CMAKE_PREFIX_... variable
-# that will then exceed the space allocated to env + args
+{
+  stdenv,
+  R,
+  makeWrapper,
+  recommendedPackages,
+  packages,
+  lndir,
+  nixpkgs,
+  lib,
+  system,
+  rPackages,
+}:
+builtins.derivation {
+  # we don't want to use a stdenv
+  # for that pulls in setupHooks
+  # and at least the cmake hook
+  # will take a long time
+  # and build a giant CMAKE_PREFIX_... variable
+  # that will then exceed the space allocated to env + args
   name = R.name + "-wrapper";
-  system =  system; 
+  system = system;
   builder = "${nixpkgs.bash}/bin/bash";
 
   args = [
     "-c"
     ''
-    findRInput() {
-        local -r pkg="$1"
-        echo "findRInput $pkg"
-        if [[ ''${already_handled[$pkg]} != "1" ]]; then
-          already_handled[$pkg]="1"
-          if test -d $pkg/library/; then
-            echo "Found lib in $pkg"
-            ${nixpkgs.coreutils}/bin/ln -s $pkg/library/* $out/lib/R/library/
+      findRInput() {
+          local -r pkg="$1"
+          echo "findRInput $pkg"
+          if [[ ''${already_handled[$pkg]} != "1" ]]; then
+            already_handled[$pkg]="1"
+            if test -d $pkg/library/; then
+              echo "Found lib in $pkg"
+              ${nixpkgs.coreutils}/bin/ln -s $pkg/library/* $out/lib/R/library/
+            fi
+            if test -f $pkg/nix-support/propagated-user-env-packages; then
+            for input in `${nixpkgs.coreutils}/bin/cat $pkg/nix-support/propagated-user-env-packages`; do
+                    findRInput $input
+                done
+            fi
+            # depends on the nixpkgs version which file is being used..
+            if test -f $pkg/nix-support/propagated-build-inputs; then
+            for input in `${nixpkgs.coreutils}/bin/cat $pkg/nix-support/propagated-build-inputs`; do
+                    findRInput $input
+                done
+            fi
+
           fi
-          if test -f $pkg/nix-support/propagated-user-env-packages; then
-          for input in `${nixpkgs.coreutils}/bin/cat $pkg/nix-support/propagated-user-env-packages`; do
-                  findRInput $input
-              done
-          fi
-          # depends on the nixpkgs version which file is being used..
-          if test -f $pkg/nix-support/propagated-build-inputs; then
-          for input in `${nixpkgs.coreutils}/bin/cat $pkg/nix-support/propagated-build-inputs`; do
-                  findRInput $input
-              done
-          fi
+        }
 
-        fi
-      }
+        ${nixpkgs.coreutils}/bin/mkdir $out/lib/R/library -p
 
-      ${nixpkgs.coreutils}/bin/mkdir $out/lib/R/library -p
+        declare -A already_handled
+        # find out what packages we need to symlink
 
-      declare -A already_handled
-      # find out what packages we need to symlink
+        for input in `${nixpkgs.coreutils}/bin/cat $nativeBuildInputsPath`; do
+            findRInput  $input
+        done
+        # make wrappers setting env variables
+        cd ${R}/bin
+        export PATH=${nixpkgs.coreutils}/bin:$PATH
+        export SHELL=${nixpkgs.bash}/bin/bash
+        source ${nixpkgs.makeWrapper}/nix-support/setup-hook # make available
+        for exe in *; do
 
-      for input in `${nixpkgs.coreutils}/bin/cat $nativeBuildInputsPath`; do
-          findRInput  $input
-      done
-      # make wrappers setting env variables
-      cd ${R}/bin
-      export PATH=${nixpkgs.coreutils}/bin:$PATH
-      export SHELL=${nixpkgs.bash}/bin/bash
-      source ${nixpkgs.makeWrapper}/nix-support/setup-hook # make available
-      for exe in *; do
-
-        makeWrapper "${R}/bin/$exe" "$out/bin/$exe" \
-          --set R_LIBS_SITE $out/lib/R/library \
-          --set LC_ALL C
-      done
+          makeWrapper "${R}/bin/$exe" "$out/bin/$exe" \
+            --set R_LIBS_SITE $out/lib/R/library \
+            --set LC_ALL C
+        done
 
     ''
   ];
 
-
-  buildInputs = [ R nixpkgs.which ];
-  nativeBuildInputs = [ makeWrapper R ] ++ recommendedPackages ++ packages;
+  buildInputs = [R nixpkgs.which];
+  nativeBuildInputs = [makeWrapper R] ++ recommendedPackages ++ packages;
   passAsFile = ["nativeBuildInputs"];
 
   #paths = [ R nixpkgs.which ] ++ shards;
@@ -76,4 +85,10 @@ builtins.derivation {
   #   # prefer wrapper over the package
   #   priority = (R.meta.priority or 0) - 1;
   # };
-} // {recommendedPackages = recommendedPackages;} # poor man's passthrough
+}
+// {
+  recommendedPackages = recommendedPackages;
+  rPackages = rPackages;
+}
+# poor man's passthrough
+
